4561e3d2fee67cfe22488f5ad1a68aedcf6a8b8d
BetannySilva <betannyalexandradasilva@gmail.com>
2026-01-31 15:24:19 -0300

    Fix FastAPI start for Railway

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
 import uvicorn
 
-port = int(os.environ.get("PORT", 8000))
+
+from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
